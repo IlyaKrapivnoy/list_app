2717057aca32d267c4533b3aec57116a6304bc89
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-07-29 19:48:34 +0300

    type action

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -8,13 +8,21 @@ interface UserState {
   error: null | boolean;
 }
 
+interface UserAction {
+  type: string;
+  payload?: any;
+}
+
 const initialState: UserState = {
   users: [],
   loading: false,
   error: null,
 };
 
-export const userReducer = (state = initialState, action): UserState => {
+export const userReducer = (
+  state = initialState,
+  action: UserAction
+): UserState => {
   switch (action.type) {
     case FETCH_USERS:
       return { loading: true, error: null, users: [] };
